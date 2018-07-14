ea5f04dad363f7e4ea2a079afcd8a77302401438
SRAUFactory <srau.factory@gmail.com>
2018-07-14 10:03:03 +0900

    feat: コード修正
    以下を対応。
    
    * コード中のコメント
    * 現在日が13日以外に当月の13日に変換するようロジックを追加

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import "fmt"
 import "time"
 
 func main() {
+	// 現在日取得
 	t := time.Now()
 	printDate(t)
 
-	t = time.Date(t.Year(), t.Month(), 13, t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
-	printDate(t)
+	if t.Day() != 13 {
+		// 一度、当月の13日に変換する
+		t = time.Date(t.Year(), t.Month(), 13, t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
+		printDate(t)
+	}
 
+	// 指定年数分の日付検出
 	for i := 0; i < 3*12; i++ {
 		t = t.AddDate(0, 1, 0)
 		if t.Weekday() == 5 {
