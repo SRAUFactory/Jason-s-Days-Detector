72e892aa898a69619306f9eff8ee230b47547c61
SRAUFactory <srau.factory@gmail.com>
2018-07-14 10:19:55 +0900

    feat: デバッグコードを整理
    ログも追加

--- main.go
+++ main.go
@@ -13,24 +13,20 @@ func main() {
 
 	// 現在日取得
 	t := time.Now()
-	printDate(t)
+	fmt.Println(t)
 
 	if t.Day() != 13 {
 		// 一度、当月の13日に変換する
 		t = time.Date(t.Year(), t.Month(), 13, t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
-		printDate(t)
+		fmt.Println(t)
 	}
 
 	// 指定年数分の日付検出
+	fmt.Println("ジェイソンが出現する日です")
 	for i := 0; i < *year*12; i++ {
 		t = t.AddDate(0, 1, 0)
 		if t.Weekday() == 5 {
-			printDate(t)
+			fmt.Println(t)
 		}
 	}
-}
-
-func printDate(t time.Time) {
-	fmt.Print(t)
-	fmt.Println(t.Weekday())
 }
