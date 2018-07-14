57155e2ab3ce41748b5417fe624041b7e0b6734d
SRAUFactory <srau.factory@gmail.com>
2018-07-14 09:59:55 +0900

    add: func to print to date

--- main.go
+++ main.go
@@ -5,18 +5,20 @@ import "time"
 
 func main() {
 	t := time.Now()
-	fmt.Print(t)
-	fmt.Println(t.Weekday())
+	printDate(t)
 
 	t = time.Date(t.Year(), t.Month(), 13, t.Hour(), t.Minute(), t.Second(), 0, time.UTC)
-	fmt.Print(t)
-	fmt.Println(t.Weekday())
+	printDate(t)
 
 	for i := 0; i < 3*12; i++ {
 		t = t.AddDate(0, 1, 0)
 		if t.Weekday() == 5 {
-			fmt.Print(t)
-			fmt.Println(t.Weekday())
+			printDate(t)
 		}
 	}
+}
+
+func printDate(t time.Time) {
+	fmt.Print(t)
+	fmt.Println(t.Weekday())
 }
